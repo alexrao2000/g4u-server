b9b1288b8280548054b1a06a80e7dac8db8fcdbf
Alex <alexrao20002@gmail.com>
2025-07-13 20:28:43 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uuid
+
+app = FastAPI()
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=['http://localhost:5173/'],
+  allow_credentials=True,
+  allow_methods=['*'],
+  allow_headers=['*'],
+)
+
+@app.get('/')
+def read_root():
+  return {"Hello": "World"}
+
+@app.get('/referral-code')
+def get_referral_code():
+  referral_code = uuid.uuid4().hex[:8]
+  return {"code": referral_code}
