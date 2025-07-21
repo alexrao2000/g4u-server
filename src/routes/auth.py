d05126f9864568bb5bcc0873a33fbd508661e186
Alex <alexrao20002@gmail.com>
2025-07-21 12:24:52 -0700

    Added wip error handling in auth.py

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -17,19 +17,23 @@ class UserModel(BaseModel):
 @router.post('/register')
 async def register(data: UserModel, db: Session=Depends(get_db)):
 
-  id = str(uuid.uuid4())
+  try:
+    id = str(uuid.uuid4())
 
-  pw_bytes = data.password.encode('utf-8')
-  salt = bcrypt.gensalt()
-  hashed_bytes = bcrypt.hashpw(pw_bytes, salt)
-  hashed_password = hashed_bytes.decode('utf-8')
+    pw_bytes = data.password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hashed_bytes = bcrypt.hashpw(pw_bytes, salt)
+    hashed_password = hashed_bytes.decode('utf-8')
 
-  user = User(uuid=id, email=data.email, password=hashed_password)
+    user = User(uuid=id, email=data.email, password=hashed_password)
 
-  db.add(user)
-  db.commit()
-  db.refresh(user)
+    db.add(user)
+    db.commit()
+    db.refresh(user)
 
-  print("ID:", id, "Email:", data.email, "Password:", hashed_password)
+    # print("ID:", id, "Email:", data.email, "Password:", hashed_password)
 
-  return {"message": "Success!"}
+    return {"message": "Success!"}
+  
+  except:
+    return {"message": "Error: Failed to create user"}
